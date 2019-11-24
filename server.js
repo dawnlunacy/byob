6571f7c3505ce000ec868b52ec050ec69e377ea6
dawnlunacy <rudd.lacy@gmail.com>
2019-11-23 17:35:09 -0700

    Add inital get request for all artists

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 var fs = require('fs');
 const cors = require('cors');
-var dataset1
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Music'
@@ -12,8 +19,17 @@ app.get('/', (request, response) => {
   response.send('View artist and their albums')
 })
 
+app.get('/api/v1/artists', (request, response) => {
+  database('artists')
+  .select()
+  .then(artists => {
+    response.status(200).json(artists)
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
-
-app.use(cors());
