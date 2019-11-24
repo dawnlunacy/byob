052d548b1d804890eadd142b9608c0a5d1ed2267
dawnlunacy <rudd.lacy@gmail.com>
2019-11-23 17:53:55 -0700

    Add queries for get requests for individual artist by name and for individual album information

--- server.js
+++ server.js
@@ -41,6 +41,44 @@ app.get('/api/v1/albums', (request, response) => {
   });
 });
 
+app.get('/api/v1/artists/:artistName', (request, response) => {
+  const { artistName } = request.params;
+  database('artists')
+  .where({ artist_name : artistName})
+  .then(artist => {
+    if(artist.length === 0) {
+      response.status(404)
+      .json({error: ` Sorry could not find an artist with a name of ${artist} . Please check your query and try again. If artist does not exist in our database, please feel free to make a contribution.` })
+    }
+    response.status(200).json(artist[0])
+  })
+  .then(artists => {
+    response.status(200).json(artists)
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
+app.get('/api/v1/albums/:albumName', (request, response) => {
+  const { albumName } = request.params;
+  database('albums')
+  .where({ album_name : albumName})
+  .then(album => {
+    if(album.length === 0) {
+      response.status(404)
+      .json({error: ` Sorry could not find an album with a name of ${album} . Please check your query and try again. If album information does not exist in our database, please feel free to make a contribution.` })
+    }
+    response.status(200).json(album[0])
+  })
+  .then(albums => {
+    response.status(200).json(albums)
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
