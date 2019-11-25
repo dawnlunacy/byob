c2c280375d8c47250be4fcd8dec73b47e7977a2c
dawnlunacy <rudd.lacy@gmail.com>
2019-11-25 03:09:07 -0700

    Add informatino for heroku

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,17 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true,
+  },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
   }
 };
